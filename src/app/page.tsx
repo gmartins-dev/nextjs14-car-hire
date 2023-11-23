a1acfadd576b040a1439d22b7113b00fba973504
gmartins-dev <guilhermemm.dev@gmail.com>
2023-11-23 21:15:06 +0000

    fix: styling fix to mobile on the index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-gray-800 text-white">
-      <div className="flex flex-row w-full h-full">
+      <div className="flex justify-center items-center flex-col gap-8 w-full h-full sm:flex-row">
         <div className="flex items-center justify-center w-1/2">
           <Link href="/users/register">
             <Button bgColor="bg-blue-600 hover:bg-blue-700 transition-colors duration-200">👤 User Registration</Button>
